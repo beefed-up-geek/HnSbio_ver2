b684b4606f3472525ec5b3863dce34c7702ffbbb
drdr3040 <110738697+drdr3040@users.noreply.github.com>
2024-11-22 23:48:42 +0900

    android resolve

--- metro.config.js
+++ metro.config.js
@@ -6,13 +6,16 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const customConfig = {
-  resolver: {
-    assetExts: ['bin'], // 기존 확장자 목록에 'bin' 추가
-  },
-};
-
 module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
+  const defaultConfig = await getDefaultConfig(__dirname); // 기본 설정 가져오기
+
+  const customConfig = {
+    resolver: {
+      // 기존 확장자를 유지하면서 'bin' 확장자 추가
+      assetExts: [...defaultConfig.resolver.assetExts, 'bin'],
+    },
+  };
+
+  // 기본 설정과 사용자 정의 설정 병합
   return mergeConfig(defaultConfig, customConfig);
 })();
