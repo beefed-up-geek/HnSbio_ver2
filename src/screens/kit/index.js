f3a9a987251fb11a797029dc6dcf2561009d6ccf
drdr3040 <110738697+drdr3040@users.noreply.github.com>
2024-11-22 17:26:22 +0900

    kit

--- src/screens/kit/index.js
+++ src/screens/kit/index.js
@@ -20,7 +20,6 @@ import theme from '../../theme'; // 개발 규칙: 폰트 적용
 import styles from './styles.js'; //스타일 불러오기 // 개발 규칙: stylesheet 분리
 import {useState, useEffect} from 'react';
 import {Alert} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const width_ratio = Dimensions.get('screen').width / 390; // 개발 규칙: 상대 크기 적용
 const height_ratio = Dimensions.get('screen').height / 844; // 개발 규칙: 상대 크기 적용
