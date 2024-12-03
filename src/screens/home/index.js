744e953edb07a08793d63aed3007c712fa408174
clrdl <163401793+clrdl@users.noreply.github.com>
2024-12-04 04:41:03 +0900

    style for kidney_info

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -19,9 +19,19 @@ import {
 import DevButton from '../../components/devButton.js';
 import styles from './styles.js'; // 스타일 분리
 
+import {
+  parseDateString,
+  parseHealthCheckupDate,
+  formatDate,
+  calculateAge,
+  calculateGFR,
+  getLatestGFR,
+} from '../../components/dataUtils';
+
 const HomeScreen = () => {
   const navigation = useNavigation();
   const route = useRoute();
+
   // 사용자 정보를 저장할 상태 변수들
   const [name, setName] = useState('');
   const [gender, setGender] = useState('');
@@ -38,9 +48,7 @@ const HomeScreen = () => {
   });
   const [latestBloodTest, setLatestBloodTest] = useState(null);
   const [latestGFR, setLatestGFR] = useState(null);
-  const [estimatedKidneyFunction, setEstimatedKidneyFunction] = useState(null);
   const [latestCheckupDate, setLatestCheckupDate] = useState('');
-  const [checkCompletedToday, setCheckCompletedToday] = useState(false);
   const [latestRecordSource, setLatestRecordSource] = useState('');
 
   // 홈화면 메인 글자를 위한 상태변수
@@ -50,31 +58,9 @@ const HomeScreen = () => {
   const [daysToNextKitTest, setDaysToNextKitTest] = useState(null);
   const [latestKitTest, setLatestKitTest] = useState(null);
   const [lastKitTestDaysAgo, setLastKitTestDaysAgo] = useState(null);
-  const [nextCheckupText2, setNextCheckupText2] = useState('');
+  const [checkCompletedToday, setCheckCompletedToday] = useState(false);
 
   const characterOpacity = useState(new Animated.Value(1))[0];
-
-  const calculateGFR = creatinine => {
-    const age = calculateAge(birthdate);
-    const isFemale = gender === 'female';
-    let gfr = 175 * Math.pow(creatinine, -1.154) * Math.pow(age, -0.203);
-    if (isFemale) {
-      gfr *= 0.742;
-    }
-    return Math.round(gfr);
-  };
-
-  const calculateAge = birthdate => {
-    const birthDate = new Date(birthdate);
-    const today = new Date();
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
-
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
-    }
-    return age;
-  };
 
   const getKidneyImage = gfrValue => {
     if (gfrValue >= 90) {
@@ -90,59 +76,20 @@ const HomeScreen = () => {
     }
   };
 
-  const formatDate = date => {
-    if (!(date instanceof Date) || isNaN(date)) {
-      return '';
-    }
-    const year = date.getFullYear();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const day = ('0' + date.getDate()).slice(-2);
-    return `${year}.${month}.${day}`;
-  };
-
-  const parseDateString = dateString => {
-    if (!dateString) return null;
-
-    // dateString can be "YYYY/MM/DD HH:mm:ss" or "YYYY/MM/DD"
-    const [datePart, timePart] = dateString.trim().split(' ');
-    const [year, month, day] = datePart.split(/[-/]/).map(Number);
-
-    let hours = 0,
-      minutes = 0,
-      seconds = 0;
-    if (timePart) {
-      [hours, minutes, seconds] = timePart.split(':').map(Number);
-    }
-
-    const date = new Date(year, month - 1, day, hours, minutes, seconds);
-    return isNaN(date.getTime()) ? null : date;
-  };
-
-  const parseHealthCheckupDate = (year, dateString) => {
-    // dateString format: "MMDD"
-    const month = parseInt(dateString.substring(0, 2), 10) - 1; // Months are 0-based
-    const day = parseInt(dateString.substring(2), 10);
-
-    return new Date(parseInt(year, 10), month, day);
-  };
-
-  // Calculate days until the next kit test
+  // Functions for kit test calculations
   const calculateNextKitTest = () => {
     if (alarmEnabled && startDate && repeatInterval) {
       const currentDate = new Date();
       const start = new Date(startDate);
       const repeatDays = parseInt(repeatInterval);
 
-      // Calculate the number of periods (cycles) since the start date
       const diffTime = currentDate.getTime() - start.getTime();
       const diffDays = Math.floor(diffTime / (1000 * 3600 * 24));
       const periodsElapsed = Math.floor(diffDays / repeatDays);
 
-      // Calculate the next test date
       const nextTestDate = new Date(start.getTime());
       nextTestDate.setDate(start.getDate() + (periodsElapsed + 1) * repeatDays);
 
-      // Calculate days left until the next test date
       const daysLeft = Math.ceil(
         (nextTestDate - currentDate) / (1000 * 3600 * 24),
       );
@@ -153,83 +100,77 @@ const HomeScreen = () => {
     }
   };
 
-  // Calculate days since the last kit test
   const calculateLastKitTest = () => {
     if (latestKitTest) {
-      // 마지막 검사 날짜 파싱
       const lastTestDate = parseDateString(latestKitTest.datetime);
-      // 시간 요소 제거 (자정으로 설정)
       lastTestDate.setHours(0, 0, 0, 0);
 
-      // 현재 날짜 및 시간 요소 제거
       const currentDate = new Date();
       currentDate.setHours(0, 0, 0, 0);
 
-      // 시간 차이 계산
       const timeDiff = currentDate - lastTestDate;
       const daysAgo = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
       if (daysAgo >= 0) {
         setLastKitTestDaysAgo(daysAgo);
       } else {
-        // 미래 날짜인 경우 (예상치 못한 상황)
         setLastKitTestDaysAgo(null);
       }
     } else {
-      setLastKitTestDaysAgo(null); // 이전 테스트가 없음
+      setLastKitTestDaysAgo(null);
     }
   };
 
-  // homeScreenData가 변경될 때마다 데이터 업데이트
-  useEffect(() => {
-    if (route.params?.homeScreenData) {
-      const data = route.params.homeScreenData;
-      setName(data.name);
-      setGender(data.gender);
-      setHeight(data.height);
-      setWeight(data.weight);
-      setBirthdate(data.birthdate);
-      setNickname(data.nickname);
-      setChronicKidneyDisease(data.chronic_kidney_disease);
-      setUnderlyingDisease(data.underlying_disease);
+  // // homeScreenData가 변경될 때마다 데이터 업데이트
+  // useEffect(() => {
+  //   if (route.params?.homeScreenData) {
+  //     const data = route.params.homeScreenData;
+  //     setName(data.name);
+  //     setGender(data.gender);
+  //     setHeight(data.height);
+  //     setWeight(data.weight);
+  //     setBirthdate(data.birthdate);
+  //     setNickname(data.nickname);
+  //     setChronicKidneyDisease(data.chronic_kidney_disease);
+  //     setUnderlyingDisease(data.underlying_disease);
 
-      if (data.healthCheckup && data.healthCheckup.length > 0) {
-        const sortedHealthCheckup = data.healthCheckup.sort((a, b) => {
-          const dateA = new Date(
-            a.resCheckupYear,
-            a.resCheckupDate.substring(0, 2) - 1,
-            a.resCheckupDate.substring(2),
-          );
-          const dateB = new Date(
-            b.resCheckupYear,
-            b.resCheckupDate.substring(0, 2) - 1,
-            b.resCheckupDate.substring(2),
-          );
-          return dateB - dateA;
-        });
+  //     if (data.healthCheckup && data.healthCheckup.length > 0) {
+  //       const sortedHealthCheckup = data.healthCheckup.sort((a, b) => {
+  //         const dateA = new Date(
+  //           a.resCheckupYear,
+  //           a.resCheckupDate.substring(0, 2) - 1,
+  //           a.resCheckupDate.substring(2),
+  //         );
+  //         const dateB = new Date(
+  //           b.resCheckupYear,
+  //           b.resCheckupDate.substring(0, 2) - 1,
+  //           b.resCheckupDate.substring(2),
+  //         );
+  //         return dateB - dateA;
+  //       });
 
-        const latestCheckup = sortedHealthCheckup[0];
-        const serumCreatinine = parseFloat(latestCheckup.resSerumCreatinine);
-        if (!isNaN(serumCreatinine) && serumCreatinine > 0) {
-          let estimatedFunction;
-          if (data.gender === 'male') {
-            estimatedFunction = (0.9 / serumCreatinine) * 100;
-          } else {
-            estimatedFunction = (0.7 / serumCreatinine) * 100;
-          }
-          setEstimatedKidneyFunction(Math.round(estimatedFunction));
-        }
+  //       const latestCheckup = sortedHealthCheckup[0];
+  //       const serumCreatinine = parseFloat(latestCheckup.resSerumCreatinine);
+  //       if (!isNaN(serumCreatinine) && serumCreatinine > 0) {
+  //         let estimatedFunction;
+  //         if (data.gender === 'male') {
+  //           estimatedFunction = (0.9 / serumCreatinine) * 100;
+  //         } else {
+  //           estimatedFunction = (0.7 / serumCreatinine) * 100;
+  //         }
+  //         setEstimatedKidneyFunction(Math.round(estimatedFunction));
+  //       }
 
-        const dateString = `${
-          latestCheckup.resCheckupYear
-        }.${latestCheckup.resCheckupDate.substring(
-          0,
-          2,
-        )}.${latestCheckup.resCheckupDate.substring(2)}`;
-        setLatestCheckupDate(dateString);
-      }
-    }
-  }, [route.params?.homeScreenData]);
+  //       const dateString = `${
+  //         latestCheckup.resCheckupYear
+  //       }.${latestCheckup.resCheckupDate.substring(
+  //         0,
+  //         2,
+  //       )}.${latestCheckup.resCheckupDate.substring(2)}`;
+  //       setLatestCheckupDate(dateString);
+  //     }
+  //   }
+  // }, [route.params?.homeScreenData]);
 
   useEffect(() => {
     calculateNextKitTest();
@@ -260,7 +201,6 @@ const HomeScreen = () => {
           setStartDate(startDate);
           setRepeatInterval(repeatInterval);
         } else {
-          // Set default values if pushNotificationSettings doesn't exist
           setAlarmEnabled(false);
           setStartDate(null);
           setRepeatInterval(null);
@@ -285,97 +225,20 @@ const HomeScreen = () => {
           } else {
             setLatestKitTest(null);
           }
-        }
-
-        const combinedResults = [];
-
-        // Process blood_test_result
-        if (
-          parsedData.blood_test_result &&
-          parsedData.blood_test_result.length > 0
-        ) {
-          parsedData.blood_test_result.forEach(test => {
-            if (test.date) {
-              const date = parseDateString(test.date);
-              if (date) {
-                combinedResults.push({
-                  source: 'blood_test',
-                  date,
-                  data: test,
-                });
-              } else {
-                console.warn(`Invalid date in blood_test_result: ${test.date}`);
-              }
-            } else {
-              console.warn('Missing date in blood_test_result entry:', test);
-            }
-          });
-        }
-
-        // Process healthCheckup
-        if (parsedData.healthCheckup && parsedData.healthCheckup.length > 0) {
-          parsedData.healthCheckup.forEach(checkup => {
-            const date = parseHealthCheckupDate(
-              checkup.resCheckupYear,
-              checkup.resCheckupDate,
-            );
-            if (!isNaN(date)) {
-              combinedResults.push({
-                source: 'health_checkup',
-                date,
-                data: checkup,
-              });
-            } else {
-              console.warn(
-                `Invalid date in healthCheckup: ${checkup.resCheckupYear}-${checkup.resCheckupDate}`,
-              );
-            }
-          });
-        }
-
-        // Find the most recent record
-        if (combinedResults.length > 0) {
-          // Sort the combined results by date in descending order
-          combinedResults.sort((a, b) => b.date - a.date);
-
-          const latestRecord = combinedResults[0];
-
-          // Set the date for display
-          setLatestCheckupDate(formatDate(latestRecord.date));
-
-          if (latestRecord.source === 'blood_test') {
-            setLatestBloodTest(latestRecord.data);
-
-            // Use GFR from the blood test result, or calculate it
-            if (latestRecord.data.GFR) {
-              setLatestGFR(latestRecord.data.GFR);
-            } else if (latestRecord.data.creatinine) {
-              const calculatedGFR = calculateGFR(latestRecord.data.creatinine);
-              setLatestGFR(calculatedGFR);
-            } else {
-              setLatestGFR(null);
-            }
-          } else if (latestRecord.source === 'health_checkup') {
-            // Use GFR from health checkup, or calculate it
-            const serumCreatinine = parseFloat(
-              latestRecord.data.resSerumCreatinine,
-            );
-            if (!isNaN(serumCreatinine) && serumCreatinine > 0) {
-              let gfrValue;
-              if (latestRecord.data.resGFR) {
-                gfrValue = parseFloat(latestRecord.data.resGFR);
-              } else {
-                gfrValue = calculateGFR(serumCreatinine);
-              }
-              setLatestGFR(gfrValue);
-            } else {
-              setLatestGFR(null);
-            }
-          }
         } else {
-          setLatestGFR(null);
-          setLatestCheckupDate(null);
+          setLatestKitTest(null);
         }
+
+        // Get latest GFR
+        const {latestGFR, latestCheckupDate, latestRecordSource} = getLatestGFR(
+          parsedData,
+          parsedData.birthdate,
+          parsedData.gender,
+        );
+
+        setLatestGFR(latestGFR);
+        setLatestCheckupDate(latestCheckupDate);
+        setLatestRecordSource(latestRecordSource);
       }
     } catch (error) {
       console.error('Error loading user data from AsyncStorage:', error);
@@ -400,25 +263,6 @@ const HomeScreen = () => {
     loadUserData(); // Load user data when the component mounts
     checkDailyCompletionStatus(); // Also check daily completion status
   }, []);
-
-  useEffect(() => {
-    let newNextCheckupText2 = '';
-    if (alarmEnabled && daysToNextKitTest !== null) {
-      newNextCheckupText2 = daysToNextKitTest + '일 남았습니다';
-    } else {
-      if (latestKitTest) {
-        if (lastKitTestDaysAgo !== null) {
-          newNextCheckupText2 = lastKitTestDaysAgo + '일 전입니다';
-        } else {
-          newNextCheckupText2 = '오늘입니다';
-        }
-      } else {
-        newNextCheckupText2 =
-          '아직 검사를 하지 않았어요.\n콩팥 건강 관리를 시작해보세요';
-      }
-    }
-    setNextCheckupText2(newNextCheckupText2);
-  }, [alarmEnabled, daysToNextKitTest, lastKitTestDaysAgo, latestKitTest]);
 
   return (
     <LinearGradient
--- src/screens/home/kidney_info/styles.js
+++ src/screens/home/kidney_info/styles.js
@@ -1,18 +1,132 @@
 // src\screens\home\kidney_info\styles.js
-import { StyleSheet, Dimensions } from 'react-native';
+
+import {StyleSheet, Dimensions} from 'react-native';
 import theme from '../../../theme'; // 개발 규칙: 폰트 적용
+
 const width_ratio = Dimensions.get('screen').width / 390; // 개발 규칙: 상대 크기 적용
 const height_ratio = Dimensions.get('screen').height / 844; // 개발 규칙: 상대 크기 적용
 
 const styles = StyleSheet.create({
-    button: {
-        marginTop: 20 * height_ratio,
-        backgroundColor: '#7596FF',
-        height: 50,
-        width: 100,
-        borderRadius: 8 * width_ratio,
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+
+  statusCard: {
+    marginHorizontal: 18 * width_ratio,
+    flexDirection: 'row',
+    marginTop: 24 * height_ratio,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#E3E3E3',
+    borderRadius: 13,
+    paddingVertical: 30 * height_ratio,
+    paddingHorizontal: 22 * width_ratio,
+    gap: 25 * width_ratio,
+  },
+  statusChartImage: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+  },
+  textWrapper: {
+    width: 160,
+    alignSelf: 'center',
+    gap: 12 * height_ratio,
+  },
+  lightText: {
+    ...theme.fonts.Light,
+    fontSize: 14,
+    color: '#7F7F7F',
+  },
+  MiddleText: {
+    ...theme.fonts.Regular,
+    fontSize: 14 * width_ratio,
+    color: '#49494F',
+  },
+  gfrText: {
+    ...theme.fonts.SemiBold,
+    fontSize: 16 * width_ratio,
+    color: '#49494F',
+  },
+
+  sectionTitle: {
+    marginLeft: 24 * width_ratio,
+    marginTop: 42 * height_ratio,
+    ...theme.fonts.SemiBold,
+    fontSize: 16 * width_ratio,
+    color: '#49494F',
+    marginBottom: 12 * height_ratio,
+  },
+
+  riskLevels: {
+    marginHorizontal: 18 * width_ratio,
+    borderWidth: 1,
+    borderColor: '#E3E3E3',
+    borderRadius: 13,
+    paddingVertical: 50,
+    alignSelf: 'center',
+  },
+  riskGraph: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 10,
+    alignItems: 'flex-end',
+  },
+  riskItem: {
+    alignItems: 'center',
+    flex: 1,
+  },
+
+  // 단계별 riskImage 스타일
+  riskImageNormal: {
+    height: 120,
+    width: 56,
+    resizeMode: 'contain',
+  },
+  riskImageCaution: {
+    height: 102,
+    width: 56,
+    resizeMode: 'contain',
+  },
+  riskImageModerate: {
+    height: 87,
+    width: 56,
+    resizeMode: 'contain',
+  },
+  riskImageSevere: {
+    height: 71,
+    width: 56,
+    resizeMode: 'contain',
+  },
+  riskImageCritical: {
+    height: 54,
+    width: 56,
+    resizeMode: 'contain',
+  },
+  descriptionContainer: {
+    marginTop: 20,
+    alignItems: 'flex-start',
+    paddingHorizontal: 20,
+  },
+
+  selectedRiskLabel: {
+    ...theme.fonts.SemiBold,
+    fontSize: 18,
+    color: '#49494F'
+  },
+  gfrCriteria: {
+    ...theme.fonts.Medium,
+    fontSize: 14,
+    marginTop: 10,
+    color: '#49494F'
+  },
+  descriptionText: {
+    ...theme.fonts.Regular,
+    fontSize: 14,
+    marginTop: 4,
+  },
 });
-  
+
 export default styles;
