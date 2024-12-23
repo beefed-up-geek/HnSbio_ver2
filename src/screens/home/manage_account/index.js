9f8f9c768e4e8bd1cf34798b989210ac8dd9cfef
Tae Yoon Kwack <62469125+beefed-up-geek@users.noreply.github.com>
2024-12-23 20:07:37 +0900

    Update index.js

--- src/screens/home/manage_account/index.js
+++ src/screens/home/manage_account/index.js
@@ -1,5 +1,3 @@
-// src\screens\home\manage_account\index.js
-
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Image, BackHandler, Alert } from 'react-native';
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
@@ -19,10 +17,10 @@ const ManageAccountScreen = () => {
         const userData = await AsyncStorage.getItem('user');
         if (userData) {
           const parsedUser = JSON.parse(userData);
-          
+
           // provider 변환
           let displayProvider = '';
-          switch(parsedUser.provider) {
+          switch (parsedUser.provider) {
             case 'kakao':
               displayProvider = '카카오';
               break;
@@ -88,6 +86,57 @@ const ManageAccountScreen = () => {
     }
   };
 
+  const handleDeleteAccountConfirmation = () => {
+    Alert.alert(
+      '회원탈퇴',
+      '회원 탈퇴 시 모든 정보가 삭제됩니다. 정말로 탈퇴하시겠습니까?',
+      [
+        { text: '아니오', style: 'cancel' },
+        { text: '예', onPress: handleDeleteAccount },
+      ]
+    );
+  };
+
+  const handleDeleteAccount = async () => {
+    try {
+      const userData = await AsyncStorage.getItem('user');
+      if (userData) {
+        const { providerId } = JSON.parse(userData);
+  
+        const response = await fetch('http://98.82.55.237/user_info/deleteUser', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ providerId }),
+        });
+  
+        if (response.ok) {
+          await AsyncStorage.removeItem('user');
+          Alert.alert('회원탈퇴가 완료되었습니다.');
+          // 앱 재시작
+          BackHandler.exitApp();
+        } else {
+          // 응답 콘텐츠 확인
+          const contentType = response.headers.get('content-type');
+          if (contentType && contentType.includes('application/json')) {
+            const errorData = await response.json();
+            console.error('회원탈퇴 실패:', errorData);
+            Alert.alert('회원탈퇴에 실패했습니다. 다시 시도해주세요.');
+          } else {
+            const errorText = await response.text();
+            console.error('회원탈퇴 실패:', errorText);
+            Alert.alert('회원탈퇴 중 서버 오류가 발생했습니다. 다시 시도해주세요.');
+          }
+        }
+      }
+    } catch (error) {
+      console.error('회원탈퇴 중 오류가 발생했습니다:', error);
+      Alert.alert('회원탈퇴 중 오류가 발생했습니다. 다시 시도해주세요.');
+    }
+  };
+  
+
   return (
     <View style={styles.container}>
       <View style={styles.detailsContainer}>
@@ -104,7 +153,7 @@ const ManageAccountScreen = () => {
             style={styles.goIcon}
           />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.detailLastRow}>
+        <TouchableOpacity style={styles.detailLastRow} onPress={handleDeleteAccountConfirmation}>
           <Text style={styles.detailLabel}>회원 탈퇴</Text>
           <Image
             source={require('../../../images/home/my_profile/go.png')}
