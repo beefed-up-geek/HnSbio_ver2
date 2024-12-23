fbf113ab708bea8d9466bf050fe4ad2c2788bdb2
Choi Hansong <gksthd001@g.skku.edu>
2024-12-24 01:54:19 +0900

    상세페이지 주의사항 글자 잘리는 거

--- src/screens/medicine/medicine_specifics/styles.js
+++ src/screens/medicine/medicine_specifics/styles.js
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
     },
     tabText: {
         color: '#7596FF',
-        fontSize: 13 * width_ratio,
+        fontSize: 13 * height_ratio,
         ...theme.fonts.Medium,
     },
     activeTabText: {
