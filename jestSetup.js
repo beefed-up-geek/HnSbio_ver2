6c2011f2db2b2b0c80aa7e14a43d7ec9cdde39c9
Choi Hansong <gksthd001@g.skku.edu>
2025-01-07 11:43:27 +0900

    jest.config.js 파일 수정 & jestSetup.js 파일 생성

---
+++ jestSetup.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler/jestSetup';
+
+// Jest에서 기본 모듈 mock 처리
+jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
+
+jest.mock('react-native-gesture-handler', () => {
+  return {
+    Swipeable: jest.fn(),
+    DrawerLayout: jest.fn(),
+    State: {},
+    Directions: {},
+    GestureHandlerRootView: ({ children }) => children,
+    createNativeWrapper: jest.fn(),
+    attachGestureHandler: jest.fn(),
+    dropGestureHandler: jest.fn(),
+    updateGestureHandler: jest.fn(),
+    enableExperimentalWebImplementation: jest.fn(),
+  };
+});
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
