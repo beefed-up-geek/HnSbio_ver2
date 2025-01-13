0679b512f73a0b6b794430d0c1b1d560b89cd443
drdr3040 <110738697+drdr3040@users.noreply.github.com>
2025-01-13 16:38:22 +0900

    버그 수정

--- src/screens/kit/styles.js
+++ src/screens/kit/styles.js
@@ -133,7 +133,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: theme.colors.white || '#fff',
     borderRadius: 10 * width_ratio,
-    padding: 15 * width_ratio,
+    padding: 16 * width_ratio,
     marginBottom: 15 * height_ratio,
     shadowColor: '#000',
     shadowOffset: {width: 0, height: 2 * height_ratio},
